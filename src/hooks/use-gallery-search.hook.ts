88cef4202807bff6c5f41d18d3ab6a799d950834
Damian Aruj <aruj.damian@gmail.com>
2023-02-06 02:37:51 +0200

    added error message and refetch

--- src/hooks/use-gallery-search.hook.ts
+++ src/hooks/use-gallery-search.hook.ts
@@ -12,19 +12,20 @@ const useGallerySearch = ({ searchAfterChars, initialSearchQuery }: UseGallerySe
     const [canSearch, setCanSearch] = useState(!!initialSearchQuery)
     const initValues = useRef({ searchAfterChars, initialSearchQuery })
 
-    const { isLoading, data, fetchNextPage, isFetching, isSuccess, isFetched, isRefetching } = useInfiniteQuery({
-        queryKey: ['search-gifs', search],
-        queryFn: async ({ queryKey, pageParam = 0 }: QueryFunctionContext<[string, string], number>) => {
-            setCanSearch(false)
-            const search = queryKey[1]
-            return await fetchGifs(search, pageParam)
-        },
-        enabled: canSearch,
-        getNextPageParam: (lastPage) => {
-            const { offset, total_count } = lastPage.pagination
-            return total_count > offset ? lastPage.pagination.offset / LIMIT + 1 : undefined
-        },
-    })
+    const { isLoading, data, fetchNextPage, isFetching, isSuccess, isFetched, isRefetching, isError, refetch } =
+        useInfiniteQuery({
+            queryKey: ['search-gifs', search],
+            queryFn: async ({ queryKey, pageParam = 0 }: QueryFunctionContext<[string, string], number>) => {
+                setCanSearch(false)
+                const search = queryKey[1]
+                return await fetchGifs(search, pageParam)
+            },
+            enabled: canSearch,
+            getNextPageParam: (lastPage) => {
+                const { offset, total_count } = lastPage.pagination
+                return total_count > offset ? lastPage.pagination.offset / LIMIT + 1 : undefined
+            },
+        })
     const onSearch = useCallback((searchTerm: string) => {
         if (searchTerm.length >= initValues.current.searchAfterChars) {
             setSearch(searchTerm)
@@ -48,7 +49,7 @@ const useGallerySearch = ({ searchAfterChars, initialSearchQuery }: UseGallerySe
         [data?.pages, isSuccess, search.length],
     )
 
-    return { onSearch, onPaginate, images, isLoading, isFetching, isSuccess, isFetched, isRefetching }
+    return { onSearch, onPaginate, images, isLoading, isFetching, isSuccess, isFetched, isRefetching, isError, refetch }
 }
 
 export default useGallerySearch
--- src/pages/GalleryPage/GalleryPage.tsx
+++ src/pages/GalleryPage/GalleryPage.tsx
@@ -6,7 +6,7 @@ import useGallerySearch from '../../hooks/use-gallery-search.hook'
 const START_SEARCHING_AFTER_CHARS = 3
 
 const GalleryPage = () => {
-    const { onSearch, onPaginate, images, isRefetching } = useGallerySearch({
+    const { onSearch, onPaginate, images, isRefetching, isError, refetch } = useGallerySearch({
         searchAfterChars: START_SEARCHING_AFTER_CHARS,
         // the initialSearchQuery will make the user always see something in the UI even if he clears the search.
         // I believe showing random gifs makes a lot of sense here because it makes the user to stay in the page longer.
@@ -17,6 +17,20 @@ const GalleryPage = () => {
         <div className={styles['gallery-page']}>
             <SearchInput onSearch={onSearch} />
             <div className={styles['content']}>
+                {isError && (
+                    <p className={styles['error']}>
+                        We've found an error, plz{' '}
+                        <a
+                            href='#/'
+                            onClick={(evt) => {
+                                evt.preventDefault()
+                                refetch()
+                            }}
+                        >
+                            retry
+                        </a>
+                    </p>
+                )}
                 <GridContainer images={images} isLoading={isRefetching} handlePaginate={onPaginate} />
             </div>
         </div>
